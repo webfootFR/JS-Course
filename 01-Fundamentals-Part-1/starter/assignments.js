fc2fc3fca318fffb073aeca173724653e1218068
webfootFR <120749560+webfootFR@users.noreply.github.com>
2023-12-14 12:04:04 +0100

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+/*
+let country = "France";
+let continent = "Europe";
+let population = " sixty six million";
+
+console.log(country, continent, population);
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,84 @@
+//          let js = "amazing"
+    
+// console.log(40 + 8 + 23 - 10);
+         
+// let firstname = "Mike";
+// console.log(firstname);
+// console.log(firstname);
+// console.log(firstname);
+
+// let javascript ="fun";
+// console.log(javascript);
+
+// let age = 30;
+
+// console.log(age);
+
+// age = 31;
+
+// console.log(age);
+
+//Math operators
+// const now = 2038
+
+// const ageJonas = now - 1990;
+// const ageSarah = now - 2008;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 + 2
+// const firstName = "Mike";
+// const lastName = "Murphy"
+// console.log(firstName + " " + lastName);
+
+// // //Assignment operators
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10 = 25
+// x *= 4;  //  x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
+
+// const now = 2038
+
+// const ageJonas = now - 1990;
+// const ageSarah = now - 2008;
+
+// // Comparios operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 1);
+
+// console.log(now - 1991 > now - 2008);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10
+// console.log (x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge);
+    
+// Challenge 1
+// let markMass = 78;
+// let johnMass = 92;
+// let markHeight = 1.69;
+// let johnHeight = 1.95;
+// let mass;
+// let bmi;
+// let bmi2;
+// bmi = johnMass / johnHeight ** 2 
+// bmi2 = markMass / markHeight ** 2
+// console.log(bmi)
+// console.log(bmi2)
+
+// console.log(bmi2 > bmi)
+
+const firstName = "Mike";
+const  job = " IT";
+const birthYear = 1961;
+const year = 2037;
+
+const mike = " I'm " + firstName + ', a ' + (year - birthYear) + ' years old' + job + '!';
+console.log(mike)
+
+const mikeNew = ` I'm ${firstName}, a ${year - birthYear} yeard old ${job}!`;
+console.log(mikeNew)
